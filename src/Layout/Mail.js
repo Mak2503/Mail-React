f05228c684fba0b033981b544e10e6be63f5e869
Mak2503 <ak2955702@gmail.com>
2022-03-10 15:57:27 +0530

    update mail reading setup

--- src/Layout/Mail.js
+++ src/Layout/Mail.js
@@ -1,5 +1,26 @@
 import React from "react";
 
-export const Mail = () => {
-  return <div></div>;
+export const Mail = ({ mails }) => {
+  console.log(mails);
+  return (
+    <div className="mails">
+      {mails.sender !== undefined ? (
+        <div>
+          <div className="mails-sub">{mails.subject}</div>
+          <div className="mails-card">
+            <div className="mails-sender">{mails.sender}</div>
+            <div className="mails-content">{mails.content}</div>
+          </div>
+        </div>
+      ) : (
+        <div className="empty-mails">
+          <img
+            src="https://outlook-1.cdn.office.net/owamail/20220225004.03/resources/images/illustration_mail-ba59df675486de5cd7383ae665b0102c.svg"
+            alt="img"
+          />
+          <div>Select an item to read</div>
+        </div>
+      )}
+    </div>
+  );
 };
--- src/Layout/MailList.js
+++ src/Layout/MailList.js
@@ -4,32 +4,43 @@ import "./Layout.css";
 
 export const MailList = (props) => {
   const { folderMails } = props;
-  console.log(folderMails);
   const [openedMail, setOpenedMail] = useState({});
-
   const clickMail = (mail) => {
     setOpenedMail(mail);
     mail.unread = false;
   };
+  // const flaggedMails = [];
+  // const filterFlagged = () => {
+  //   flaggedMails.push(folderMails.every((mail) => mail.isFlagged === true));
+  // };
+  // console.log(flaggedMails);
+  const setFlag = (mail) => {
+    mail.isFlagged = true;
+  };
+
   return (
-    <div className="mail">
-      <div className="mail-filter">Filter</div>
-      {folderMails.map((mail) => {
-        return (
-          <div
-            className={mail.unread ? "unopened-mail-list" : "opened-mail-list"}
-            key={mail.mId}
-            onClick={() => clickMail(mail)}
-          >
-            <div className="mail-list-head">
-              <div className="mail-list-content">{mail.sender}</div>
-              <span>flag</span>
+    <div className="mail-section">
+      <div className="mail">
+        <div className="mail-filter">Filter</div>
+        {folderMails.map((mail) => {
+          return (
+            <div
+              className={
+                mail.unread ? "unopened-mail-list" : "opened-mail-list"
+              }
+              key={mail.mId}
+              onClick={() => clickMail(mail)}
+            >
+              <div className="mail-list-head">
+                <div className="mail-list-content">{mail.sender}</div>
+                <span onClick={() => setFlag(mail)}>flag</span>
+              </div>
+              <div className="mail-list-content">{mail.subject}</div>
+              <div className="mail-list-content">{mail.content}</div>
             </div>
-            <div className="mail-list-content">{mail.subject}</div>
-            <div className="mail-list-content">{mail.content}</div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       <Mail mails={openedMail} />
     </div>
   );
